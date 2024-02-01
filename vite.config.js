38866a3fa8f8734ca6cd7b87508262b52e061cc5
ChidiRNweke <chidi125@gmail.com>
2024-02-01 20:38:49 +0100

    Add templating system based on vite
    This change is necessary to support building the blog pages. Each page will be a .md file that gets converted to html. These will get the same header, footer and head injected into them.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,26 @@
-export default {
-    base: '/blog/'
-}
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+const htmlInjectPlugin = {
+    name: 'html-inject',
+    transformIndexHtml: {
+        order: 'pre',
+        handler(html) {
+            const head = fs.readFileSync(path.resolve(__dirname, 'src/templates/head.html'), 'utf-8');
+            const header = fs.readFileSync(path.resolve(__dirname, 'src/templates/header.html'), 'utf-8');
+            const footer = fs.readFileSync(path.resolve(__dirname, 'src/templates/footer.html'), 'utf-8');
+            return html
+                .replace('<!--head-->', head)
+                .replace('<!--header-->', header)
+                .replace('<!--footer-->', footer);
+        }
+    }
+};
+
+export default defineConfig({
+    plugins: [{ ...htmlInjectPlugin, enforce: 'post' }],
+    base: "/blog/"
+});
+
+
